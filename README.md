31b2edd55be33c68863db0ddf563a37414669352
杨鹏 <yangpeng1@dgg.net>
2019-07-02 15:14:34 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+<h1 align="center">Supports</h1>
+
+handle with array/config/log/guzzle etc.
+
+## About log
+
+### Register
+
+#### Method 1
+
+A application logger can extends `Tlwl\Supports\Log` and modify `createLogger` method, the method must return instance of `Monolog\Logger`.
+
+```PHP
+use Tlwl\Supports\Logger;
+use Monolog\Logger;
+
+class ApplicationLogger extends Logger
+{
+    /**
+     * Make a default log instance.
+     *
+     * @author tlwl <tian_wolf@sina.com>
+     *
+     * @return Logger
+     */
+    public static function createLogger()
+    {
+        $handler = new StreamHandler('./log.log');
+        $handler->setFormatter(new LineFormatter("%datetime% > %level_name% > %message% %context% %extra%\n\n"));
+
+        $logger = new Logger('tlwl.private_number');
+        $logger->pushHandler($handler);
+
+        return $logger;
+    }
+}
+```
+
+#### Method 2
+
+Or, just init the log service with:
+
+```PHP
+use Tlwl\Supports\Logger;
+
+protected function registerLog()
+{
+    $logger = Logger::createLogger($file, $identify, $level);
+
+    Logger::setLogger($logger);
+}
+```
+
+### Usage
+
+After registerLog, you can use Log service:
+
+```PHP
+use Tlwl\Supports\Logger;
+
+Logger::debug('test', ['test log']);
+```
+
+## LICENSE 
+[MIT](https://github.com/tian-wolf/supports/blob/master/LICENSE)
---
+++ src/Logger.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Tlwl\Supports;
+
+use Exception;
+use Psr\Log\LoggerInterface;
+use Monolog\Logger as MLogger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\RotatingFileHandler;
+
+/**
+ * @method static void emergency($message, array $context = array())
+ * @method static void alert($message, array $context = array())
+ * @method static void critical($message, array $context = array())
+ * @method static void error($message, array $context = array())
+ * @method static void warning($message, array $context = array())
+ * @method static void notice($message, array $context = array())
+ * @method static void info($message, array $context = array())
+ * @method static void debug($message, array $context = array())
+ * @method static void log($message, array $context = array())
+ */
+class Logger
+{
+    /**
+     * Logger instance.
+     *
+     * @var LoggerInterface
+     */
+    protected static $logger;
+
+    /**
+     * Forward call.
+     *
+     * @author 杨鹏 <yangpeng1@dgg.net>
+     *
+     * @param string $method
+     * @param array  $args
+     *
+     * @throws \Exception
+     *
+     * @return mixed
+     */
+    public static function __callStatic($method, $args)
+    {
+        return forward_static_call_array([self::getLogger(), $method], $args);
+    }
+
+    /**
+     * Forward call.
+     *
+     * @author 杨鹏 <yangpeng1@dgg.net>
+     *
+     * @param string $method
+     * @param array  $args
+     *
+     * @throws \Exception
+     *
+     * @return mixed
+     */
+    public function __call($method, $args)
+    {
+        return call_user_func_array([self::getLogger(), $method], $args);
+    }
+
+    /**
+     * Return the logger instance.
+     *
+     * @author 杨鹏 <yangpeng1@dgg.net>
+     *
+     * @throws \Exception
+     *
+     * @return LoggerInterface
+     */
+    public static function getLogger()
+    {
+        return self::$logger ?: self::$logger = self::createLogger();
+    }
+
+    /**
+     * Set logger.
+     *
+     * @author 杨鹏 <yangpeng1@dgg.net>
+     *
+     * @param LoggerInterface $logger
+     */
+    public static function setLogger(LoggerInterface $logger)
+    {
+        self::$logger = $logger;
+    }
+
+    /**
+     * Tests if logger exists.
+     *
+     * @author 杨鹏 <yangpeng1@dgg.net>
+     *
+     * @return bool
+     */
+    public static function hasLogger()
+    {
+        return self::$logger ? true : false;
+    }
+
+    /**
+     * Make a default log instance.
+     *
+     * @author 杨鹏 <yangpeng1@dgg.net>
+     *
+     * @param string     $file
+     * @param string     $identify
+     * @param int|string $level
+     * @param string     $type
+     * @param int        $max_files
+     *
+     * @throws Exception
+     *
+     * @return MLogger
+     */
+    public static function createLogger($file = null, $identify = 'tlwl.supports', $level = MLogger::DEBUG, $type = 'daily', $max_files = 30)
+    {
+        $file = is_null($file) ? sys_get_temp_dir().'/logs/'.$identify.'.log' : $file;
+
+        $handler = $type === 'single' ? new StreamHandler($file, $level) : new RotatingFileHandler($file, $max_files, $level);
+
+        $handler->setFormatter(
+            new LineFormatter("[%datetime%] %channel%.%level_name% : %message% %context% %extra%".PHP_EOL, null, false, true)
+        );
+
+        $logger = new MLogger($identify);
+        $logger->pushHandler($handler);
+
+        return $logger;
+    }
+}
